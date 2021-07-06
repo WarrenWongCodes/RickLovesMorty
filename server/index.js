90a3a4b083a7ee760fe482683862cb682e2909eb
Warren Wong <warren.wong4@gmail.com>
2021-07-06 19:22:10 -0400

    Express server setup  #Please enter the commit message for your changes. Lines starting

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
+const port = 4000;
+
+app.use(express.json());
+app.use(morgan('dev'));
+
+app.get('/', (req, res) => {
+  res.send('Hello world');
+});
+
+app.listen(port, () => console.log(`Listening on http://localhost:${port}`));
