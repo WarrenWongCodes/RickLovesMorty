5a4dd3210597168039d1e976c2251917a9de12d8
Warren Wong <warren.wong4@gmail.com>
2021-07-06 19:16:51 -0400

    Change bundle.js file path

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: path.resolve(__dirname, '/client/index.jsx'),
+  entry: path.resolve(__dirname, '/client/src/index.jsx'),
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'public'),
