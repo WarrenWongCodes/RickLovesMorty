09301d540bfa205af4ffe72dd34d96e245a810fa
Warren Wong <warren.wong4@gmail.com>
2021-07-07 08:25:19 -0400

    Implement get all episodes on render

--- client/src/components/app.jsx
+++ client/src/components/app.jsx
@@ -1,6 +1,23 @@
-import React, { useState } from 'react';
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
 
 export default function () {
+  const [episodes, setEpisodes] = useState({});
+
+  const getAllEpisodes = () => {
+    axios
+      .get('/episodes')
+      .then((results) => {
+        console.log(results.data);
+        setEpisodes(results.data);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  useEffect(() => {
+    getAllEpisodes();
+  }, []);
+
   return (
     <main role="main">
       <h1>Rick and Morty Forever</h1>
