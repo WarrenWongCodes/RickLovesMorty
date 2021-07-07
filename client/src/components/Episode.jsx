11ea1ae2c43192e407477dff0ca533900e8f7793
Warren Wong <warren.wong4@gmail.com>
2021-07-07 09:48:40 -0400

    Episode Cards complete
    Cards with learn more button functionality.

--- client/src/components/Episode.jsx
+++ client/src/components/Episode.jsx
@@ -1,5 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 
 export default function Episode({ episode }) {
-  return <section>{episode.name}</section>;
+  const [isLearnMoreClicked, setClick] = useState(false);
+
+  const learnMoreHandler = () => {
+    setClick(!isLearnMoreClicked);
+  };
+
+  return (
+    <Card>
+      <CardActionArea>
+        <CardContent>
+          {isLearnMoreClicked ? (
+            <>
+              <Typography>Aired on: {episode.air_date}</Typography>
+              <Typography>Episode created on: {episode.created}</Typography>
+            </>
+          ) : (
+            <>
+              <Typography>{episode.name}</Typography>
+              <Typography>{episode.episode}</Typography>
+            </>
+          )}
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button onClick={learnMoreHandler} size="small" color="primary">
+          Learn More
+        </Button>
+      </CardActions>
+    </Card>
+  );
 }
