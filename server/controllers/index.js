02b96bc459b85b580646f24cfbd4fa812e01b8f1
Warren Wong <warren.wong4@gmail.com>
2021-07-07 16:45:59 -0400

    Implement get all favorites request handler

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -57,4 +57,44 @@ module.exports = {
       res.status(400).json(err);
     }
   },
+  getAllFavoriteCharacters: async (req, res) => {
+    try {
+      const response = await Character.find({});
+      res.status(200).json(
+        response.map((character) => {
+          const {
+            id,
+            name,
+            status,
+            species,
+            type,
+            gender,
+            origin,
+            location,
+            image,
+            episode,
+            url,
+            created,
+          } = character;
+
+          return {
+            id,
+            name,
+            status,
+            species,
+            type,
+            gender,
+            origin: JSON.parse(origin),
+            location: JSON.parse(location),
+            image,
+            episode,
+            url,
+            created,
+          };
+        })
+      );
+    } catch (err) {
+      console.log(err);
+    }
+  },
 };
--- server/routes/index.js
+++ server/routes/index.js
@@ -2,12 +2,15 @@ const router = require('express').Router();
 const {
   retrieveAllEpisodes,
   retrieveAllCharacters,
+  getAllFavoriteCharacters,
   addFavoriteCharacter,
 } = require('../controllers/index.js');
 
 router.get('/episodes', retrieveAllEpisodes);
 
 router.get('/characters', retrieveAllCharacters);
+
+router.get('/favorites', getAllFavoriteCharacters);
 
 router.post('/character', addFavoriteCharacter);
 
