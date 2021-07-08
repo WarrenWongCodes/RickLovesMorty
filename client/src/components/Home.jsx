d79a5710d5362376a086aa9b5564fa8da48826c3
Warren Wong <warren.wong4@gmail.com>
2021-07-08 16:41:19 -0400

    Remove extra code
    Removed code that was transferred to character page component

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,64 +1,7 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import Characters from './Characters.jsx';
+import React from 'react';
 import Blurb from './Blurb.jsx';
-import SearchBar from './SearchBar.jsx';
-import PaginationLink from './Pagination.jsx';
 
 export default function Home() {
-  const [characters, setCharacters] = useState({});
-  const [currentPage, setCurrentPage] = useState(1);
-  const [query, setQuery] = useState('');
-  const [category, setCategory] = useState('name');
-
-  const onSearch = (e) => setQuery(e.target.value);
-
-  const changeCategory = (e) => setCategory(e.target.value);
-
-  const searchHandler = () => {
-    axios
-      .get('/filter', {
-        params: {
-          category,
-          query,
-        },
-      })
-      .then(({ data }) => setCharacters(data))
-      .catch((err) => console.log(err));
-  };
-
-  const getAllCharacters = () => {
-    axios
-      .get('/characters', {
-        params: {
-          pageNum: currentPage,
-        },
-      })
-      .then(({ data }) => setCharacters(data))
-      .catch((err) => console.log(err));
-  };
-
-  const onPageChange = (e) => {
-    setCurrentPage(Number(e.target.innerText));
-    getAllCharacters();
-  };
-
-  const addToFavorites = (characterInfo) => {
-    const options = {
-      method: 'post',
-      url: '/character',
-      data: characterInfo,
-    };
-
-    axios(options)
-      .then(({ data }) => console.log(data))
-      .catch((err) => console.log(err));
-  };
-
-  useEffect(() => {
-    getAllCharacters();
-  }, [currentPage, category]);
-
   return (
     <>
       <Blurb />
