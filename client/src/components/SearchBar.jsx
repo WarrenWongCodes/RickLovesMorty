d00307388b32c866be706c574bee549ae6dc96bc
Warren Wong <warren.wong4@gmail.com>
2021-07-08 16:26:54 -0400

    Update Search Bar style

--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -1,25 +1,32 @@
 import React, { useState } from 'react';
+import Typography from '@material-ui/core/Typography';
 import TextField from '@material-ui/core/TextField';
 import SearchIcon from '@material-ui/icons/Search';
+import '../styles/searchBar.scss';
 
 export default function SearchBar({ search, click, changeCategory }) {
   return (
-    <>
-      <TextField
-        onChange={search}
-        id="outlined-search"
-        label="Search field"
-        type="search"
-        variant="outlined"
-      />
-      <TextField
-        id="outlined-select-category-native"
-        label="Category"
-        onChange={changeCategory}
-        helperText="name, status, species, type, gender"
-        variant="outlined"
-      />
-      <SearchIcon onClick={click} fontSize="large" />
-    </>
+    <div id="searchContainer">
+      <Typography fontSize="large">
+        Learn what your favorite characters are up to and collect cards!
+      </Typography>
+      <div id="searchBar">
+        <TextField
+          onChange={search}
+          id="outlined-search"
+          label="Search here ..."
+          type="search"
+          variant="outlined"
+        />
+        <TextField
+          id="outlined-select-category-native"
+          label="Category"
+          onChange={changeCategory}
+          helperText="name, status, species, type, gender"
+          variant="outlined"
+        />
+        <SearchIcon onClick={click} fontSize="large" />
+      </div>
+    </div>
   );
 }
