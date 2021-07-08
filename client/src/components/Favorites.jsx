1001259473b9d4021075b7df478995596c6b4ac9
Warren Wong <warren.wong4@gmail.com>
2021-07-08 16:55:35 -0400

    Update cards styling
    Divs text aligned to center

--- client/src/components/Favorites.jsx
+++ client/src/components/Favorites.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import Favorite from './Favorite.jsx';
 import '../styles/characters.scss';
+import '../styles/favorites.scss';
+import Typography from '@material-ui/core/Typography';
 
 export default function Favorites() {
   const [favoriteCharacters, setFavoriteCharacters] = useState([]);
@@ -33,10 +35,15 @@ export default function Favorites() {
   }, []);
 
   return (
-    <main className="characterCards" role="main">
-      {favoriteCharacters.map((character, i) => (
-        <Favorite key={i} character={character} del={deleteFavorite} />
-      ))}
-    </main>
+    <>
+      <Typography variant="h3" fontSize="large">
+        My Collection
+      </Typography>
+      <main className="characterCards" role="main">
+        {favoriteCharacters.map((character, i) => (
+          <Favorite key={i} character={character} del={deleteFavorite} />
+        ))}
+      </main>
+    </>
   );
 }
