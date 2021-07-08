5becf4d695fb64d5413f87d030c03824c5bd4df7
Warren Wong <warren.wong4@gmail.com>
2021-07-08 09:36:16 -0400

    Fix giphy title accessibility

--- client/src/components/Giphy.jsx
+++ client/src/components/Giphy.jsx
@@ -5,7 +5,7 @@ export default function Giphy() {
   return (
     <section style={{ width: '100' }}>
       <div id="giphyContainer">
-        <iframe></iframe>
+        <iframe title="Rick and Morty Giphy" aria-hidden="true"></iframe>
       </div>
     </section>
   );
