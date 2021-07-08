802f93c714d21fd9331fe5cd3d25fe75f024907c
Warren Wong <warren.wong4@gmail.com>
2021-07-08 18:24:53 -0400

    Implement episode highlight component
    Included a recent episode with iframe tag to home page.

---
+++ client/src/components/EpisodeHighlight.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default function EpisodeHighlight() {
+  return (
+    <section>
+      <iframe
+        width="800"
+        height="600"
+        src="https://www.youtube-nocookie.com/embed/DYCEXg4_lCM?&autoplay=1&mute=1"
+        title="YRick and Morty Season 5 Premiere: Mort Dinner Rick Andre"
+        frameBorder="0"
+        // allow="fullscreen accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        // allowfullscreen
+        allow="fullscreen"
+      ></iframe>
+    </section>
+  );
+}
--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Blurb from './Blurb.jsx';
+import EpisodeHighlight from './EpisodeHighlight.jsx';
 
 export default function Home() {
   return (
     <>
       <Blurb />
+      <EpisodeHighlight />
     </>
   );
 }
