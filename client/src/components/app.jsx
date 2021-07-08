7772ee08895f301b604033309a10ac61a5bfff77
Warren Wong <warren.wong4@gmail.com>
2021-07-08 16:48:26 -0400

    Include background image

--- client/src/components/app.jsx
+++ client/src/components/app.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import '../styles/main.scss';
+import '../styles/app.scss';
 import Home from './Home.jsx';
 import CharacterPage from './CharacterPage.jsx';
 import Favorites from './Favorites.jsx';
@@ -11,20 +12,22 @@ import SocialMedia from './SocialMedia';
 export default function App() {
   return (
     <Router>
-      <header className="main">
-        <NavBar />
-      </header>
-      <main className="main" role="main">
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/characters" component={CharacterPage} />
-          <Route exact path="/favorites" component={Favorites} />
-        </Switch>
-      </main>
-      <footer className="main">
-        <ScrollBar />
-        <SocialMedia />
-      </footer>
+      <div id="mainApp">
+        <header className="main">
+          <NavBar />
+        </header>
+        <main className="main" role="main">
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/characters" component={CharacterPage} />
+            <Route exact path="/favorites" component={Favorites} />
+          </Switch>
+        </main>
+        <footer className="main">
+          <ScrollBar />
+          <SocialMedia />
+        </footer>
+      </div>
     </Router>
   );
 }
