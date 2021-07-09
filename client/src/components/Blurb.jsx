dad4466975cb5419d8b87d41deaf2672294b7274
Warren Wong <warren.wong4@gmail.com>
2021-07-08 21:28:55 -0400

    Add emoji

--- client/src/components/Blurb.jsx
+++ client/src/components/Blurb.jsx
@@ -7,7 +7,8 @@ export default function Blurb() {
     <section id="blurbContainer">
       <Typography variant="h1">Rick Loves Morty</Typography>
       <Typography className="blurb" variant="h2">
-        Mad scientist meets good hearted but fretful grandson Morty Smith
+        &#128104;&#127995;&#8205;&#128300; Mad scientist meets good hearted but
+        fretful grandson Morty Smith
       </Typography>
       <Typography className="blurb" paragraph={true}>
         Stay up to date on your favorite characters and where they are in the
