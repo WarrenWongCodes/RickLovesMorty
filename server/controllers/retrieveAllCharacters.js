c337852249ad6da2b504365f7fc2f5713e086d8d
Warren Wong <warren.wong4@gmail.com>
2021-07-08 22:20:07 -0400

    Implement caching get all characters controller

--- server/controllers/retrieveAllCharacters.js
+++ server/controllers/retrieveAllCharacters.js
@@ -1,13 +1,28 @@
 const { getAllCharacters } = require('../API_Helpers/index.js');
+const redisClient = require('../redis.js');
+const DEFAULT_EXPIRATION = 1800;
 
 const retrieveAllCharacters = async (req, res) => {
   const pageNum = req.query.pageNum;
-  try {
-    const { data } = await getAllCharacters(pageNum);
-    res.status(200).json(data);
-  } catch (err) {
-    res.status(400).json(err);
-  }
+  redisClient.get(`characters?pageNum=${pageNum}`, async (err, characters) => {
+    try {
+      if (characters !== null) {
+        console.log('cache hit!');
+        return res.status(200).json(JSON.parse(characters));
+      } else {
+        console.log('cache missed');
+        const { data } = await getAllCharacters(pageNum);
+        redisClient.setex(
+          `characters?pageNum=${pageNum}`,
+          DEFAULT_EXPIRATION,
+          JSON.stringify(data)
+        );
+        res.status(200).json(data);
+      }
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  });
 };
 
 module.exports = retrieveAllCharacters;
